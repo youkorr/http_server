551d73130a0dfebfa68f0f282508042716385a42
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-08 16:07:25 +0100

    Update http_server.cpp

--- components/http_server/http_server.cpp
+++ components/http_server/http_server.cpp
@@ -1,1 +1,25 @@
+#include "http_server.h"
+
+void HttpServer::setup() {
+  if (!SD.begin()) {
+    ESP_LOGE("SD", "Échec d'initialisation de la carte SD");
+    return;
+  }
+  
+  ESP_LOGI("HTTP", "Serveur HTTP démarré sur le port 8080");
+
+  // Route pour servir un fichier audio
+  server.on("/test.flac", HTTP_GET, [](AsyncWebServerRequest *request) {
+    File file = SD.open("/test.flac");
+    if (!file) {
+      request->send(404, "text/plain", "Fichier non trouvé");
+      return;
+    }
+
+    request->send(file, "audio/flac");
+    file.close();
+  });
+
+  server.begin();
+}
 
